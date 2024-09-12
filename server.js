e71dc201686479f5bdbbeb0cc1e95e76fb972acf
BaidikiKabiraj <143425059+BaidikiKabiraj@users.noreply.github.com>
2024-09-12 05:32:30 +0530

    update server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-const path= require('path');
+const path= require("path");
 
 //configure env
 dotenv.config();
